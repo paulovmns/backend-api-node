21b8b611302f29e39ba8a3046a03dde02ce1389d
Paulo Vinicius <paulovmns@gmail.com>
2022-11-28 16:46:40 -0300

    update

--- src/application.ts
+++ src/application.ts
@@ -5,7 +5,7 @@ import routes from "./routes";
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/backendapi',{
+mongoose.connect('mongodb+srv://admin:admin55013back@backend-api.fz2bs3z.mongodb.net/?retryWrites=true&w=majority',{
     family: 4,
 });
 
